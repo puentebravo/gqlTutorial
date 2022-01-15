66cabfe586deac78facf3a5b2b6c311d48a9aa52
Puentebravo <puentematos@gmail.com>
2022-01-15 16:57:39 -0500

    updated schema with context

--- src/schema.ts
+++ src/schema.ts
@@ -1,13 +1,17 @@
 import { makeSchema } from "nexus";
 import { join } from "path";
-import * as types from "./graphql"
+import * as types from "./graphql";
 
 export const schema = makeSchema({
   types, // array of types that will comprise the schema itself
   outputs: {
-      // first output: generates a schema file, defines structure of api
+    // first output: generates a schema file, defines structure of api
     schema: join(__dirname, "..", "schema.graphql"),
-    // typescript definitions for all types in graphQL schema 
+    // typescript definitions for all types in graphQL schema
     typegen: join(__dirname, "..", "nexus-typegen.ts"),
+  },
+  contextType: { // defines context object
+    module: join(__dirname, "./context.ts"), // absolute path to the module where the interface/type is being exported
+    export: "Context", // Name of the exported interface/type in the referenced module. 
   },
 });
