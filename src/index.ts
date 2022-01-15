f08e278a0b8fe0c6300b226c151af6ebd97bbdfe
Puentebravo <puentematos@gmail.com>
2022-01-15 16:57:18 -0500

    added context to server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { ApolloServer } from "apollo-server";
 // Imports the object defined by Nexus. This dictates which api operations to support in the Api.
 import { schema } from "./schema";
+import { context } from "./context";
 
 
 export const server = new ApolloServer({
     schema,
+    context
 })
 
 
