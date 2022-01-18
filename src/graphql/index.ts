ac6f9b367d6e8aece008cd9940a23016f1d42541
Puentebravo <puentematos@gmail.com>
2022-01-18 18:33:30 -0500

    Exported User object type to server

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,4 +1,3 @@
 export * from "./Link";
-
-
 // exports everything from ./link. Remember; in node, index.js/ts is the default entry point in a folder, and thus, anything exported from here can safely be imported without delving deeper.
+export * from "./User";
