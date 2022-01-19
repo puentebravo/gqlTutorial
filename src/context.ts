69848fe2cf13b62df0e96f045fdc5fcc0135bfa1
Puentebravo <puentematos@gmail.com>
2022-01-19 18:29:19 -0500

    modified context

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import { decodeAuthHeader, AuthTokenPayload } from "./utils/clientAuth";
+import { Request } from "express";
+
+
 export const prisma = new PrismaClient();
 
-export interface Context { // Defines context interface, specifying which objects will be attached to the context object. In this instance, it's attaching the prisma client. This can be added to as a project grows.
+export interface Context { // Defines context interface, specifying which objects will be attached to the context object. In this instance, it's attaching the prisma client. It's also now defining user ID as a number, while also setting it to optional for when no users are logged in or no requests are sent without the Authorization header 
     prisma: PrismaClient
+    userId?: number
 }
 
-export const context: Context = { // Exports the context object, which can then be used by a graphQL server.
-    prisma,
-};
 
+export const context = ({ req }: {req: Request}): Context => { // No longer the simple return we had here before, this is now a function that needs to be executed to return the actual Context-typed object. 
+
+    const token = 
+    req && req.headers.authorization
+        ? decodeAuthHeader(req.headers.authorization)
+        : null
+
+    return {
+        prisma, 
+        userId: token?.userId
+    }
+}
