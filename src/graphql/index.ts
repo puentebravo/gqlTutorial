fa263d1b0d16268d2ad34b3bdae109e8b3510647
Puentebravo <puentematos@gmail.com>
2022-01-24 22:46:56 -0500

    added vote type, exported to nexus

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,3 +2,5 @@ export * from "./Link";
 // exports everything from ./link. Remember; in node, index.js/ts is the default entry point in a folder, and thus, anything exported from here can safely be imported without delving deeper.
 export * from "./User";
 export * from "./Auth";
+export * from "./Vote";
+
