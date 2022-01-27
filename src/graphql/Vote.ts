0c70fc4f226bd6d0e83a36c3dbfea6e8a6316f8d
Puentebravo <puentematos@gmail.com>
2022-01-26 19:17:56 -0500

    added Vote types/mutation, added notes

--- src/graphql/Vote.ts
+++ src/graphql/Vote.ts
@@ -1,7 +1,7 @@
 import { objectType, extendType, nonNull, intArg } from "nexus";
 import { User } from "@prisma/client";
 
-export const Vote = objectType({
+export const Vote = objectType({ // Not much new here, but it's worth noting this object is a union of two things: a link, and the user who cast the vote
   name: "Vote",
   definition(t) {
     t.nonNull.field("link", { type: "Link" });
@@ -9,7 +9,7 @@ export const Vote = objectType({
   },
 });
 
-export const VoteMutation = extendType({
+export const VoteMutation = extendType({ // This mutation returns an instance of a vote type, as seen below.
   type: "Mutation",
   definition(t) {
     t.field("vote", {
@@ -25,7 +25,7 @@ export const VoteMutation = extendType({
           throw new Error("You must be logged in to vote.");
         }
 
-        const link = await context.prisma.link.update({
+        const link = await context.prisma.link.update({ // Updates the voters field in a link with the userID of the person who voted. The connect option on line 34 connects the vote with the userID, adding that user to the many to many relationship. 
           where: {
             id: linkId,
           },
@@ -42,7 +42,7 @@ export const VoteMutation = extendType({
 
         return {
             link,
-            user: user as User
+            user: user as User  // This is notable here, as the default type returned by findUnique is User OR Null. The resolver expects User.
         }
       },
     });
