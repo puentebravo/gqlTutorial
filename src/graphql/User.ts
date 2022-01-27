3a4207ce112e3075010af10bb4280db95eba7af4
Puentebravo <puentematos@gmail.com>
2022-01-26 19:17:03 -0500

    added educational notes to file

--- src/graphql/User.ts
+++ src/graphql/User.ts
@@ -6,12 +6,21 @@ export const User = objectType({
     t.nonNull.int("id");
     t.nonNull.string("name");
     t.nonNull.string("email");
-    t.nonNull.list.nonNull.field("links", { // populates the list of links posted by the user via a database call using the id of the parent object. 
+    t.nonNull.list.nonNull.field("links", {
+      // populates the list of links posted by the user via a database call using the id of the parent object.
       type: "Link",
       resolve(parent, args, context) {
         return context.prisma.user
           .findUnique({ where: { id: parent.id } })
           .links();
+      },
+    });
+    t.nonNull.list.nonNull.field("votes", {
+      type: "Link",
+      resolve(parent, args, context) {
+        return context.prisma.user
+          .findUnique({ where: { id: parent.id } })
+          .votes();
       },
     });
   },
