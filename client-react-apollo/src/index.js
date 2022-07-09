a67cdd5b87fd68e6977c47a3a81815636f003d01
Puentebravo <puentematos@gmail.com>
2022-07-09 10:49:47 -0400

    Set up apollo client

--- client-react-apollo/src/index.js
+++ client-react-apollo/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
+import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+// Import of all necessary dependencies from apollo client - think of these as the components for the local server
+import {
+  ApolloProvider, // this and the next one down assemble the apollo server instance itself
+  ApolloClient,
+  createHttpLink, // creates a uri for the client to connect to the graphql server
+  InMemoryCache 
+} from "@apollo/client"  
+
+// declare uri for apollo/graphql server
+const httpLink = createHttpLink({
+  uri: "http:/localhost:4000"
+});  
+
+// instantiate a new Apollo client, passing in the uri above and creating a new memory cache.
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+// renders the app wrapped in the apolloprovider, which is passed the client as a prop. Neat. 
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
